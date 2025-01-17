bfc44cf5c5c7ca9b7cae821023fa430afee87497
DevOps <devops@gmail.com>
2025-01-17 23:52:15 +0530

    hi

---
+++ git_push.sh
@@ -0,0 +1,16 @@
+# Script to Push changes to Remote Git repo
+
+#!/bin/bash
+
+status="$(git status -s)"
+echo $status
+
+if [ "$status" != "" ]; then
+
+git add .
+read -p "Enter commit message : " message
+git commit -m $message
+git push
+else
+echo "Nothing to commit."
+fi
